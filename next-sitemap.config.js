e152f5cba206c6f22e1d8d9e1bc75c90adcac86b
yacinTech <jeenmatheo2@gmail.com>
2025-07-20 20:15:04 +0100

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://elynor-store.vercel.app/', // غيره إلى رابط موقعك
+  generateRobotsTxt: true, // لإنشاء ملف robots.txt تلقائياً
+  // خيارات إضافية هنا حسب حاجتك
+};
