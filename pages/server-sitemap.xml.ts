18dfced23cf75a75a73351a5cded51de9fa46ec0
yacinTech <jeenmatheo2@gmail.com>
2025-07-21 20:48:28 +0100

    done

--- pages/server-sitemap.xml.ts
+++ pages/server-sitemap.xml.ts
@@ -8,24 +8,45 @@ type Product = {
 };
 
 function generateSiteMap(products: Product[]) {
+  // روابط الصفحات الثابتة التي تريد تضمينها في السايت ماب
+  const staticPages = [
+    { loc: `${BASE_URL}/` }, // الصفحة الرئيسية
+    { loc: `${BASE_URL}/about` }, // صفحة من نحن
+    { loc: `${BASE_URL}/category/الملابس والأزياء` }, // مثال صفحة تصنيف ثابتة
+    { loc: `${BASE_URL}/category/عناية شخصية` },
+     { loc: `${BASE_URL}/category/الإلكترونيات` },
+    { loc: `${BASE_URL}/category/منتجات%20الأطفال` },
+    { loc: `${BASE_URL}/privacy` },
+  ];
+
+  // بناء روابط الصفحات الثابتة بصيغة XML
+  const staticPagesXml = staticPages.map(page => `
+    <url>
+      <loc>${page.loc}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
+  `).join('');
+
+  // بناء روابط صفحات المنتجات كما هو عندك
+  const productsXml = products.map(product => `
+    <url>
+      <loc>${BASE_URL}/${product._id}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
+  `).join('');
+
+  // جمع كل الروابط معاً في ملف السايت ماب
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${products
-      .map((product) => {
-        return `
-          <url>
-            <loc>${BASE_URL}/${product._id}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-          </url>
-        `;
-      })
-      .join('')}
+    ${staticPagesXml}
+    ${productsXml}
   </urlset>`;
 }
 
+
+
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   try {
-    // تأكد أن API_BASE يشير لنقطة نهاية المنتجات، مثلاً 'https://api.example.com/products'
     const response = await fetch(`${API_BASE}`);
     const products: Product[] = await response.json();
 
