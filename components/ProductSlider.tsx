b761fa8fa0098c49e27e2e8593d5fc6c198b5605
MedAmine720 <immedamin2@gmail.com>
2025-07-22 18:16:24 +0100

    donee

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -15,7 +15,7 @@ const ProductSlider: React.FC<ProductSliderProps> = ({ images }) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 3333,
     arrows: true,
     rtl: true, // لدعم الاتجاه من اليمين إلى اليسار
   };
