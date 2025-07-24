f97f886662c6f494f1bc5c804fe7b37cd7ff3aca
yacinTech <jeenmatheo2@gmail.com>
2025-07-24 16:07:58 +0100

    add new style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '../styles/globals.css'; // استيراد CSS العالمي
+import '../styles/globals.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import WhatsAppButton from '../components/WhatsAppButton';
 import NewsletterForm from '../components/NewsletterForm';
-
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -22,7 +22,36 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1793174024744863&ev=PageView&noscript=1"
+            alt="fb pixel"
+          />
+        </noscript>
       </Head>
+
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src='https://connect.facebook.net/en_US/fbevents.js';
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script');
+            fbq('init', '1793174024744863');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
       <Header />
       <Component {...pageProps} />
       <WhatsAppButton />
