b8714e78da0b8e6816ef39275578aaf2f840b431
yacinTech <jeenmatheo2@gmail.com>
2025-07-28 23:14:21 +0100

    add sitemap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
 
         <link
           rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
+
+
+        <link
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
           integrity="sha512-..."
           crossOrigin="anonymous"
