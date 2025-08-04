9ac1d230df7359b65e2f83c1aa28e0ea71962a43
yacinTech <jeenmatheo2@gmail.com>
2025-08-04 15:45:06 +0100

    add fcbk pixel

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -74,37 +74,30 @@ useEffect(() => {
         <link rel="canonical" href={`https://yourdomain.com/category/${encodeURIComponent(category)}`} />
       </Head>
       {category === 'منتجات الأطفال' && (
-        <>
-         <Script id="facebook-pixel" strategy="afterInteractive">
-              {`
-                !function(f,b,e,v,n,t,s) {
-                  if(f.fbq) return; n = f.fbq = function() {
-                    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                  };
-                  if(!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
-                  t = b.createElement(e); t.async = !0;
-                  t.src = v;
-                  s = b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t, s);
-                }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '610812365430824');
-                fbq('track', 'PageView');
-              `}
-            </Script>
+  <>
+    <Script id="facebook-pixel" strategy="afterInteractive">
+      {`
+        !function(f,b,e,v,n,t,s) {
+          if(f.fbq) return; n = f.fbq = function() {
+            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+          };
+          if(!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
+          t = b.createElement(e); t.async = !0;
+          t.src = v;
+          s = b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t, s);
+        }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '610812365430824');
+        fbq('track', 'PageView');
+      `}
+    </Script>
 
+    <div dangerouslySetInnerHTML={{
+      __html: `<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=610812365430824&ev=PageView&noscript=1" /></noscript>`
+    }} />
+  </>
+)}
 
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: 'none' }}
-                src="https://www.facebook.com/tr?id=610812365430824&ev=PageView&noscript=1"
-                alt="fb pixel"
-              />
-            </noscript>
-
-        </>
-      )}
       
 <div
   style={{
