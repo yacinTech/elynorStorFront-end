22758bc9fe6c892050f19b01e423197dadec3a8f
yacinTech <jeenmatheo2@gmail.com>
2025-08-04 17:32:47 +0100

    add topbanner in the categoies pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,9 +12,20 @@ import WhatsAppButton from '../components/WhatsAppButton';
 import NewsletterForm from '../components/NewsletterForm';
 import ContactSection from '../components/ContactSection';
 import { ToastContainer } from 'react-toastify';
-
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // التحقق إذا كنا في صفحة منتجات الأطفال
+  const isKidsCategory =
+    router.pathname === '/category/[category]' &&
+    decodeURIComponent(
+      typeof router.query.category === 'string'
+        ? router.query.category
+        : router.query.category?.[0] || ''
+    ) === 'منتجات الأطفال';
+
   return (
     <div style={{ overflowX: 'hidden', maxWidth: '100vw' }}>
       <Head>
@@ -34,11 +45,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             alt="fb pixel"
           />
         </noscript>
-        
-
-        <link rel="icon" href="https://elynor-store.vercel.app/og-image.jpg" type="image/jpeg" />
+        <link
+          rel="icon"
+          href="https://elynor-store.vercel.app/og-image.jpg"
+          type="image/jpeg"
+        />
       </Head>
 
+      {/* 📌 البيكسل الأول - يعمل دائمًا */}
       <Script
         id="facebook-pixel"
         strategy="afterInteractive"
@@ -57,14 +71,50 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           `,
         }}
       />
-      
-      <div style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 10000 }}>
+
+      {/* ✅ البيكسل الثاني - فقط في صفحة منتجات الأطفال */}
+      {isKidsCategory && (
+        <>
+          <Script id="facebook-pixel-kids" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src='https://connect.facebook.net/en_US/fbevents.js';
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script');
+              fbq('init', '610812365430824');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=610812365430824&ev=PageView&noscript=1"
+              alt="fb pixel"
+            />
+          </noscript>
+        </>
+      )}
+
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 10000,
+        }}
+      >
         <Header />
       </div>
-  
 
-      
       <Component {...pageProps} />
+
       <ToastContainer
         position="top-center"
         autoClose={4000}
