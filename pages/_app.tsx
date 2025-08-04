b3f2f14327f9699a5b748d9064b00d612d36721c
yacinTech <jeenmatheo2@gmail.com>
2025-08-04 16:48:55 +0100

    add fcbk pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,19 +12,9 @@ import WhatsAppButton from '../components/WhatsAppButton';
 import NewsletterForm from '../components/NewsletterForm';
 import ContactSection from '../components/ContactSection';
 import { ToastContainer } from 'react-toastify';
-import { useRouter } from 'next/router';
-
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
- const categoryParam = router.query.category;
-  const categoryStr = typeof categoryParam === 'string' ? categoryParam : categoryParam ? categoryParam[0] : '';
-  const category = decodeURIComponent(categoryStr);
-  const isKidsCategoryPage = router.asPath.includes('/category/منتجات الأطفال');
-
-
-  
   return (
     <div style={{ overflowX: 'hidden', maxWidth: '100vw' }}>
       <Head>
@@ -44,31 +34,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             alt="fb pixel"
           />
         </noscript>
-             
-             {isKidsCategoryPage && (
-              <>
-               <Script id="facebook-pixel" strategy="afterInteractive">
-                    {`
-                      !function(f,b,e,v,n,t,s) {
-                        if(f.fbq) return; n = f.fbq = function() {
-                          n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                        };
-                        if(!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
-                        t = b.createElement(e); t.async = !0;
-                        t.src = v;
-                        s = b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t, s);
-                      }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-                      fbq('init', '610812365430824');
-                      fbq('track', 'PageView');
-                    `}
-                  </Script>
-      
-      
-           
-              </>
-            )}
-
+        
 
         <link rel="icon" href="https://elynor-store.vercel.app/og-image.jpg" type="image/jpeg" />
       </Head>
@@ -91,16 +57,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           `,
         }}
       />
-    
-              <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: 'none' }}
-                src="https://www.facebook.com/tr?id=610812365430824&ev=PageView&noscript=1"
-                alt="fb pixel"
-              />
-            </noscript>
       
       <div style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 10000 }}>
         <Header />
