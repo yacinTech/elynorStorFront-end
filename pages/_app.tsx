a72333f5c02790e9a6bdcf08af37238d769cd456
yacinTech <jeenmatheo2@gmail.com>
2025-08-08 15:05:33 +0100

    add new pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -179,6 +179,37 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
       )}
 
+      {/* ✅ البيكسل الرابع لتتبع الموقع كاملاً */}
+          <Script
+            id="meta-pixel-all"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src='https://connect.facebook.net/en_US/fbevents.js';
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script');
+                fbq('init', '2473982636321790');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=2473982636321790&ev=PageView&noscript=1"
+              alt="fb pixel all site"
+            />
+          </noscript>
+
+
       <div
         style={{
           position: 'fixed',
