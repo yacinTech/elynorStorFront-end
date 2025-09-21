65258ad6057b048130d801c89b31d2e5e7ae2f48
yacinTech <jeenmatheo2@gmail.com>
2025-09-21 13:09:31 +0100

    add new component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -236,8 +236,6 @@ const isEtagerPage = slug === 'etagere';
 
 
 
-
-
 {isEtagerPage && (
   <>
     <Script
@@ -245,22 +243,15 @@ const isEtagerPage = slug === 'etagere';
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
-          !function(f,b,e,v,n,t,s){
-            if(f.fbq)return;
-            n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;
-            n.push=n;
-            n.loaded=!0;
-            n.version='2.0';
-            n.queue=[];
-            t=b.createElement(e);t.async=!0;
-            t.src='https://connect.facebook.net/en_US/fbevents.js';
-            s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)
-          }(window, document,'script');
-          
-          fbq('init', '662892656149516'); 
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '662892656149516');
           fbq('track', 'PageView');
         `,
       }}
@@ -276,6 +267,7 @@ const isEtagerPage = slug === 'etagere';
     </noscript>
   </>
 )}
+
 
 {/* ✅ البيكسل المخصص (753759500831707) */}
 {isProductsForPixel5 && (
